9e07ac1a71316f2b5a0f93808506ec36804fe731
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-03 11:14:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,10 +6,8 @@ class School
     @roster = {}
   end 
   def add_student(name,grade)
-    if roster.has_key?(grade)
-      roster[grade] << name
-    else
-      roster[grade] = [name]
+   self.roster[grade] ||=[ ]
+   self.roster[grade] << name
     end
   end 
 end
