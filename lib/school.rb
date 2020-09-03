cac29b694eea0bb26c5a75ac05cbe28a45d79498
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-03 11:19:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,13 +2,13 @@ class School
   attr_reader :name, :roster
   
   def initialize(name)
-    @name = name
-    @roster = Hash.new {|h,k| h [k] = []}
-  end 
-  def add_student(name,grade)
-   self.roster[grade] << name
-    end
-    def grade 
+     @name = name
+     @roster = Hash.new {|h,k| h [k] = []}
+     end 
+     def add_student(name,grade)
+     self.roster[grade] << name
+     end
+     def grade 
   end 
 
   
